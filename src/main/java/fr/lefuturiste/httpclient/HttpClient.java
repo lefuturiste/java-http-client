921e99052f5ef0434f6c3ceec234e8c55ca668df
Le_Futuriste <spamfree@matthieubessat.fr>
2019-05-07 19:55:45 +0200

    initial commit

---
+++ src/main/java/fr/lefuturiste/httpclient/HttpClient.java
@@ -0,0 +1,13 @@
+package fr.lefuturiste.httpclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class HttpClient
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fr/lefuturiste/httpclient/test/HttpClientTest.java
@@ -0,0 +1,20 @@
+package fr.lefuturiste.httpclient.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HttpClient.
+ */
+public class HttpClientTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
